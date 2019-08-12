83fce2c20a3317bfac9282c0d25d55a80bce3b9c
Conner Swann <conner@o1labs.org>
2019-08-12 14:55:31 -0700

    added testnet-pyramid-scheme

--- terraform/live/testnets.tf
+++ terraform/live/testnets.tf
@@ -2,9 +2,8 @@ module "testnet-pyramid-scheme" {
   source = "../modules/coda-testnet"
   net_name = "pyramid-scheme"
   net_region = "us-west-2"
-  coda_version = "195917-release-0.0.3-beta-e3b9aead-PV222430f0"
+  coda_version = "196448-release-0.0.3-beta-f027ad0c-PV96140f2c"
   seed_count = 1
   snarker_count = 1
   proposer_count = 5
 }
-
